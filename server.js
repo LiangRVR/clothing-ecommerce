de1283db92fa868ac9ffb29aac5e305e918945cd
LiangRVR <liangvillarrubia95@gmail.com>
2022-05-23 22:51:55 -0400

    fixed bug on service worker server, changed path.join into path.resolve

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.listen(port, (error) => {
 });
 
 app.get("/service-worker.js", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "build", "service-worker.js"));
+  res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
 });
 
 app.post("/payment/create", async (req, res) => {
