b07cf2dd817102eb594c4b875363e8c606e68061
LiangRVR <liangvillarrubia95@gmail.com>
2022-06-08 23:24:24 -0400

    added test to user reducer

---
+++ client/src/redux/user/__test__/user.reducer.test.js
@@ -0,0 +1,54 @@
+import userReducer from "../user.reducer";
+
+import {
+  signInSuccess,
+  signOutSuccess,
+  signInFailure,
+  signOutFailure,
+  signUpFailure,
+} from "../user.actions";
+
+import userActions from "../user.actions";
+
+const initialState = {
+  currentUser: null,
+  error: null,
+};
+
+describe("testing userReducer", () => {
+  it("should return initial state", () => {
+    expect(userReducer(undefined, {})).toEqual(initialState);
+  });
+
+  it("should set currentUser to payload on signInSucces action", () => {
+    const mockUser = { id: 1, userName: "Liang Villarrubia" };
+    expect(
+      userReducer(initialState, signInSuccess(mockUser)).currentUser
+    ).toEqual(mockUser);
+  });
+
+  it("should set currentUser to null on signOutSucces action", () => {
+    expect(userReducer(initialState, signOutSuccess()).currentUser).toBe(null);
+  });
+
+  it("should setError currentUser to null on signInFailure, signOutFailure, signUpFailure actions", () => {
+    const mockError = {
+      message: "errored",
+      error: 404,
+    };
+    expect(userReducer(initialState, signInFailure(mockError)).error).toEqual(
+      mockError
+    );
+    expect(userReducer(initialState, signOutFailure(mockError)).error).toEqual(
+      mockError
+    );
+    expect(userReducer(initialState, signUpFailure(mockError)).error).toEqual(
+      mockError
+    );
+  });
+
+  it("test",()=>{
+      console.log(JSON.stringify(userActions))
+
+  })
+});
